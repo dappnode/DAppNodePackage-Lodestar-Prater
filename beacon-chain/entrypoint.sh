72511f1e9d8aaefed282160c72ac72acaf05b9c7
Voss <alex@visnovaventures.com>
2022-09-21 06:04:08 -0500

    Adopt for StakersUI

---
+++ beacon-chain/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Concatenate EXTRA_OPTS string
+[[ -n "$CHECKPOINT_SYNC_URL" ]] && EXTRA_OPTS="${EXTRA_OPTS} --checkpointSyncUrl=${CHECKPOINT_SYNC_URL}"
+
+case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
+"goerli-geth.dnp.dappnode.eth")
+    HTTP_ENGINE="http://goerli-geth.dappnode:8551"
+    ;;
+"goerli-nethermind.dnp.dappnode.eth")
+    HTTP_ENGINE="http://goerli-nethermind.dappnode:8551"
+    ;;
+"goerli-erigon.dnp.dappnode.eth")
+    HTTP_ENGINE="http://goerli-erigon.dappnode:8551"
+    ;;
+*)
+    echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER"
+    HTTP_ENGINE=$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER
+    ;;
+esac
+
+# MEVBOOST: https://github.com/ChainSafe/lodestar/blob/unstable/docs/usage/mev-integration.md
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" == "true" ]; then
+    echo "MEVBOOST is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost-goerli.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder --builder.urls "${MEVBOOST_URL}" ${EXTRA_OPTS}"
+    else
+        echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
+    fi
+fi
+
+exec node /usr/app/node_modules/.bin/lodestar \
+    beacon \
+    --network=mainnet \
+    --jwt-secret=/jwtsecret \
+    --execution.urls=$HTTP_ENGINE \
+    --dataDir=/var/lib/data \
+    --rest \
+    --rest.address 0.0.0.0 \
+    --rest.port ${BEACON_API_PORT} \
+    --port $P2P_PORT \
+    --logFile /var/lib/data/beacon.log \
+    --logFileLevel debug \
+    --logFileDailyRotate 5 \
+    $EXTRA_OPTS
---
+++ validator/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+NETWORK="prater"
+VALIDATOR_PORT=3500
+
+# MEVBOOST: https://github.com/ChainSafe/lodestar/blob/unstable/docs/usage/mev-integration.md
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" == "true" ]; then
+    echo "MEVBOOST is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost-goerli.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder ${EXTRA_OPTS}"
+    else
+        echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
+    fi
+fi
+
+exec node /usr/app/node_modules/.bin/lodestar \
+    validator \
+    --network=${NETWORK} \
+    --suggestedFeeRecipient=${FEE_RECIPIENT_ADDRESS} \
+    --graffiti=${GRAFFITI} \
+    --keymanager true \
+    --keymanager.authEnabled false \
+    --keymanager.port 3500 \
+    --keymanager.address 0.0.0.0 \
+    --externalSigner.url=${HTTP_WEB3SIGNER} \
+    --server=${BEACON_NODE_ADDR} \
+    --logLevel=${DEBUG_LEVEL} \
+    --logFileLevel=debug \
+    --logFileDailyRotate 5 \
+    --logFile /var/lib/data/validator.log \
+    $EXTRA_OPTS
