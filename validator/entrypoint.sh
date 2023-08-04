ba5ccef0c79e6fcc108690812fbcf5622c2aa42f
Nico Flaig <nflaig@protonmail.com>
2023-08-04 09:24:09 +0200

    Update CLI flag to enable doppelganger protection

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -26,7 +26,7 @@ exec node /usr/app/node_modules/.bin/lodestar \
     --metrics.port 5064 \
     --metrics.address 0.0.0.0 \
     --externalSigner.url=${HTTP_WEB3SIGNER} \
-    --doppelgangerProtectionEnabled \
+    --doppelgangerProtection \
     --beaconNodes=${BEACON_NODE_ADDR} \
     --logLevel=${DEBUG_LEVEL} \
     --logFileLevel=debug \
